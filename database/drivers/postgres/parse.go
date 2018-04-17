d707797ef11517f7e51964b12258bd5e1c1121d4
Chen.Zhidong <zhidong.chen@jimeng.io>
2018-04-17 15:15:33 +0800

    replace \n to space in postgresql comments
    Fix: error calling makeTable: failed to render from pipe delimited bytes: record on line 4: wrong number of fields

--- database/drivers/postgres/parse.go
+++ database/drivers/postgres/parse.go
@@ -536,7 +536,8 @@ func queryColumnComments(log *log.Logger, db *sql.DB, schemaNames []string) ([]c
 		}
 
 		if c.Valid {
-			r.Comment = c.String
+			replaced := strings.Replace(c.String, "\n", " ", -1)
+			r.Comment = replaced
 			results = append(results, r)
 		}
 	}
@@ -586,7 +587,8 @@ func queryTableComments(log *log.Logger, db *sql.DB, schemaNames []string) ([]ta
 		}
 
 		if c.Valid {
-			r.Comment = c.String
+			replaced := strings.Replace(c.String, "\n", " ", -1)
+			r.Comment = replaced
 			results = append(results, r)
 		}
 	}
